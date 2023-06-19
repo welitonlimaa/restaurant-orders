e9c19445d4e9595990e8e75f0012d01e73c24633
welitonlimaa <welitonlima159@gmail.com>
2023-06-19 15:47:51 -0300

    req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,5 +1,18 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction
 
 
 # Req 1
 def test_ingredient():
+    carne = Ingredient("carne")
+    carne2 = Ingredient("carne")
+    bacon = Ingredient("bacon")
+
+    assert carne.__hash__() == carne2.__hash__()
+    assert carne.__hash__() != bacon.__hash__()
+    assert carne.__repr__() == "Ingredient('carne')"
+    assert carne.__eq__(carne2) is True
+    assert carne.__eq__(bacon) is False
+    assert carne.name == "carne"
+    assert carne.restrictions == {Restriction.ANIMAL_DERIVED,
+                                  Restriction.ANIMAL_MEAT}
